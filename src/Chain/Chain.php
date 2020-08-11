d63d8735f4c047237a540e0b09d721aa59775d19
danieletulone <danieletulone.work@gmail.com>
2020-08-11 16:32:08 +0200

    Add class for create a chain to store and run

---
+++ src/Chain/Chain.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Chain;
+
+class Chain
+{
+    private $name;
+
+    private $inputs = [];
+
+    public function __construct(string $name, array $inputs)
+    {
+        $this->name = $name;
+        $this->inputs = $inputs;
+    }
+
+    public static function create(string $name, array $inputs)
+    {
+        $option = new Chain($name, $inputs);
+        
+        return Ford::addChain($chain, $inputs);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getInputs()
+    {
+        return $this->inputs;
+    }
+
+    public function addOption(array $input)
+    {
+        array_push($this->inputs, $input); 
+    }
+}
