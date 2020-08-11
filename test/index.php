7e767dd2f0896a0a0969d6f04b59b8a77b547bb2
danieletulone <danieletulone.work@gmail.com>
2020-08-11 16:34:39 +0200

    Remove laravel collector and edit index of tests

--- test/index.php
+++ test/index.php
@@ -1,9 +1,37 @@
 <?php
 
-use Chain\Collector as ChainCollector;
+use Chain\Ford;
+use Chain\Option;
+use Chain\Chain;
 
 require ('../vendor/autoload.php');
 
-require ('collector/laravel.php');
+class A
+{
+    public function sortBy($a)
+    {
+        echo "sortBy -> $a";
 
-echo json_encode(ChainCollector::getAll());
+        return $this;
+    }
+
+    public function get()
+    {
+        echo "get <br>";
+
+        return $this;
+    }
+}
+
+$a = new A();
+
+$sortByOption = Option::create('sb', 'sortBy', 5)->addArgument('string');
+
+$getOption = Option::create('g', 'get');
+
+$chain = Chain::create('base-get');
+
+Ford::on($a)->run([
+    'sb' => 'ciao',
+    'g' => '',
+]);
